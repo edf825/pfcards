685749867481a6e5dd7bc3022d8d4fd4c83160d9
Edwin Flores <edwin@flores.geek.nz>
2024-06-09 15:26:16 +0100

    Add some deduplication and improve feat lookups

--- pfcards/app.py
+++ pfcards/app.py
@@ -12,6 +12,8 @@ from .packs import load_packs
 LOG = logging.getLogger(__name__)
 PACKS = load_packs()
 
+DEBUG_JSON = None
+
 app = FastAPI()
 
 class CardsModel(BaseModel):
@@ -33,57 +35,74 @@ def cards(cards_model: CardsModel):
         LOG.warning(msg)
         return {"name": name, "type": "error", "message": msg}
 
-    cards = []
+    cards = {}
 
     basics_fields = ["name", "class", "dualClass", "level", "ancestry", "heritage", "background", "alignment", "gender", "age", "deity", "sizeName", "keyability", "languages", "resistances"]
     basics = {k: build[k] for k in basics_fields}
     basics["ac"] = build["acTotal"]["acTotal"]
     basics["speed"] = build["attributes"]["speed"]
     basics["type"] = "stats"
-    cards.append(basics)
+    cards["basics"] = basics
 
     for caster in build.get("spellCasters", []):
         for spells in caster["spells"]:
             level = spells["spellLevel"]
             for name in spells["list"]:
-                cards.append({
+                cards[(name, "spell", caster["magicTradition"], caster["ability"], caster["name"])] = {
                     **find_in_pack(name, ["spell"]),
                     **{
                         "tradition": caster["magicTradition"],
                         "ability": caster["ability"],
                         "class": caster["name"],
                     }
-                })
+                }
 
     for tradition_name, tradition in build.get("focus", {}).items():
         for ability_name, ability in tradition.items():
             for name in ability.get("focusCantrips", []):
-                cards.append(dict(
+                cards[(name, "focusCantrips", tradition_name, ability_name)] = dict(
                     find_in_pack(name, ["spell"]),
-                        tradition=tradition_name,
-                        ability=ability_name,
-                        spell_type="Focus Cantrip",
-                    )
+                    tradition=tradition_name,
+                    ability=ability_name,
+                    spell_type="Focus Cantrip",
                 )
             for name in ability.get("focusSpells", []):
-                cards.append(dict(
+                cards[(name, "focusSpells", tradition_name, ability_name)] = dict(
                     find_in_pack(name, ["spell"]),
-                        tradition=tradition_name,
-                        ability=ability_name,
-                        spell_type="Focus Spell",
-                    )
+                    tradition=tradition_name,
+                    ability=ability_name,
+                    spell_type="Focus Spell",
                 )
 
     for feat in build["feats"]:
         name = feat[0]
-        cards.append(find_in_pack(feat[0], ["feat", "heritage"]))
+        desc = (feat + [None] * 5)[4]  # e.g. "Witch Feat 4", "Elf Feat 9"
 
-    return JSONResponse(cards)
+        card = None
+        if desc is not None:
+            suffix = f" ({desc.split()[0]})"
+            card = find_in_pack(feat[0] + suffix, ["feat", "heritage"])
+
+        if card is None or card["type"] == "error":
+            card = find_in_pack(feat[0], ["feat", "heritage"])
+
+        cards[(name, "feats")] = card
+
+    return JSONResponse(list(cards.values()))
 
 def json_url(_id):
     return f"https://pathbuilder2e.com/json.php?id={_id}"
 
+def maybe_read_debug_json():
+    if DEBUG_JSON is not None:
+        with open(DEBUG_JSON) as fd:
+            return json.load(fd)["build"]
+
 def get_json(_id):
+    debug = maybe_read_debug_json()
+    if debug is not None:
+        return debug
+
     rsp = requests.get(json_url(_id), headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"})
     rsp.raise_for_status()
     obj = rsp.json()
